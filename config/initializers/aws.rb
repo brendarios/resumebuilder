b447a61fa790e2bb2e6aaf2f03688a0c47454cb1
Brenda Rios <riosbrenda@gmail.com>
2018-07-11 13:13:54 -0700

    removed aws credentials

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
 Aws.config.update({
    region: 'us-west-2',
-   credentials: Aws::Credentials.new('AKIAJPRV6RIA4L5QMMPA', 'FoZa4mxIBRJ6wrcSIqh94LloMvKsXppY/HMf5h3u'),
+   credentials: Aws::Credentials.new(''),
  })
