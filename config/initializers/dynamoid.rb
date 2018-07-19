09b6ff140c56276132115b8a9d49f7fd8b614bd0
Brenda Rios <riosbrenda@gmail.com>
2018-07-19 10:26:09 -0700

    trying different code to connect to DDB

--- config/initializers/dynamoid.rb
+++ config/initializers/dynamoid.rb
@@ -1,10 +1,15 @@
 require 'dynamoid'
 
   Dynamoid.configure do |config|
+    config.access_key = 'AWS_ACCESS_KEY_ID'
+    config.secret_key = 'AWS_SECRET_ACCESS_KEY'
+    config.region = 'us-west-2'
     config.namespace = nil # To namespace tables created by Dynamoid from other tables you might have. Set to nil to avoid namespacing.
-    config.region = ENV['AWS_REGION'] || 'us-west-2'
-    if ENV['DYNAMODB_ENDPOINT'].present?
-      config.endpoint = ENV['DYNAMODB_ENDPOINT'] # [Optional]. If provided, it communicates with the DB listening at the endpoint. This is useful for testing with [Amazon Local DB] (http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Tools.DynamoDBLocal.html).
-    end
+    # config.region = ENV['AWS_REGION'] || 'us-west-2'
+    # if ENV['DYNAMODB_ENDPOINT'].present?
+    #   config.endpoint = ENV['DYNAMODB_ENDPOINT'] # [Optional]. If provided, it communicates with the DB listening at the endpoint. This is useful for testing with [Amazon Local DB] (http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Tools.DynamoDBLocal.html).
+    # end
 
   end
+
+# DYNAMODB_ENDPOINT = http://localhost:8000
