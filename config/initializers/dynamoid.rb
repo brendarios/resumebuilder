20fb35f3884eb40d745e2d917dea9f7166f559a7
Brenda Rios <riosbrenda@gmail.com>
2018-07-19 11:03:44 -0700

    added new endpoint

--- config/initializers/dynamoid.rb
+++ config/initializers/dynamoid.rb
@@ -5,10 +5,12 @@ require 'dynamoid'
     config.secret_key = 'AWS_SECRET_ACCESS_KEY'
     config.region = 'us-west-2'
     config.namespace = nil # To namespace tables created by Dynamoid from other tables you might have. Set to nil to avoid namespacing.
-    # config.region = ENV['AWS_REGION'] || 'us-west-2'
-    # if ENV['DYNAMODB_ENDPOINT'].present?
-    #   config.endpoint = ENV['DYNAMODB_ENDPOINT'] # [Optional]. If provided, it communicates with the DB listening at the endpoint. This is useful for testing with [Amazon Local DB] (http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Tools.DynamoDBLocal.html).
-    # end
+    config.region = ENV['AWS_REGION'] || 'us-west-2'
+    if ENV['DYNAMODB_ENDPOINT'].present?
+      config.endpoint = ENV['DYNAMODB_ENDPOINT'] # [Optional]. If provided, it communicates with the DB listening at the endpoint. This is useful for testing with [Amazon Local DB] (http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Tools.DynamoDBLocal.html).
+    else
+      config.endpoint = "arn:aws:dynamodb:us-west-2:572896613617:table/resumes"
+    end
 
   end
 
