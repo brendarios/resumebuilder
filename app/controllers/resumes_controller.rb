97d7574b6512452d108cf6f96e43de0ac84c9b36
Brenda Rios <riosbrenda@gmail.com>
2018-07-20 17:16:15 -0700

    debugging why the sqs receives msg but does not do anything

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -148,8 +148,8 @@ class ResumesController < ApplicationController
     def build_resume
       if session[:uid]
         @resume = Resume.find_by_user_uid(session[:uid])
-      else
-        @resume = Resume.find_by_id(params[:id])
+      # else
+      #   @resume = Resume.find_by_id(params[:id])
       end
       ResumeConverterJob.perform_later @resume
     end
