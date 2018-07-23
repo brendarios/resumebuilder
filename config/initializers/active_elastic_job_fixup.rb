f0b6e8f426f0e5e1643bb319fbaba7618fb6d793
Brenda Rios <riosbrenda@gmail.com>
2018-07-23 13:39:38 -0700

    going back to state trying to debug worker deployment.

--- config/initializers/active_elastic_job_fixup.rb
+++ config/initializers/active_elastic_job_fixup.rb
@@ -1,8 +1,8 @@
-class AEJFixupRailtie < Rails::Railtie
-
-  if config.respond_to?(:active_elastic_job)
-    config.active_elastic_job.process_jobs = true
-  end
-
-  puts "Process jobs: #{config&.active_elastic_job&.process_jobs}"
-end
+# class AEJFixupRailtie < Rails::Railtie
+#
+#   if config.respond_to?(:active_elastic_job)
+#     config.active_elastic_job.process_jobs = true
+#   end
+#
+#   puts "Process jobs: #{config&.active_elastic_job&.process_jobs}"
+# end
