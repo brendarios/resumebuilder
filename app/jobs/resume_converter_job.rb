5a40b683e46ef02301cd4ae0322a2fb1cb2292a7
Brenda Rios <riosbrenda@gmail.com>
2018-07-24 11:15:21 -0700

    trying again gem to convert to pdf.

--- app/jobs/resume_converter_job.rb
+++ app/jobs/resume_converter_job.rb
@@ -8,7 +8,8 @@ class ResumeConverterJob < ApplicationJob
     resumes.each do |resume|
       puts "hello world"
 
-      pdf = WickedPdf.new.pdf_from_url('http://builderesume-env-1.pnfmvibna9.us-west-2.elasticbeanstalk.com/resumes/:id')
+      # resume_pdf = WickedPdf.new.pdf_from_url('http://builderesume-env-1.pnfmvibna9.us-west-2.elasticbeanstalk.com/resumes/:id')
+      resume_pdf = WickedPdf.new.pdf_from_string('<h1>Hello There!</h1>')
 
       save_path = '/tmp/file2.pdf'
       File.open(save_path, 'wb') do |file|
@@ -16,7 +17,7 @@ class ResumeConverterJob < ApplicationJob
         file << pdf
       end
       s3 = Aws::S3::Resource.new(region: 'us-west-2')
-      s3.bucket('resumesproject').object('pdf').upload_file(save_path)
+      s3.bucket('resumesproject').object('resume_pdf').upload_file(save_path)
 
       # delete file from computer
       File.delete(save_path)
